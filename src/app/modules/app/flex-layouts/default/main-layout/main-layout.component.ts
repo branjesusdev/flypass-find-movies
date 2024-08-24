8e3a1134a75031cc1ea9c93127e4621dab39984d
Brandol Vargas <brandol.fontecha@moviired.co>
2024-08-24 00:02:58 -0500

    With � for branjesusdev �

--- src/app/modules/app/flex-layouts/default/main-layout/main-layout.component.ts
+++ src/app/modules/app/flex-layouts/default/main-layout/main-layout.component.ts
@@ -11,10 +11,6 @@ import { MainHeaderComponent } from '@fxLayouts/default/main-header/main-header.
   imports: [MainHeaderComponent, RouterModule],
   template: `
     <main class="min-h-screen relative h-full w-full bg-black">
-      <!-- <div class="absolute inset-0 pointer-events-none z-0
-      bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]
-      [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div> -->
-
       <div
         class="absolute z-0 inset-0 pointer-events-none bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"
       ></div>
@@ -26,6 +22,8 @@ import { MainHeaderComponent } from '@fxLayouts/default/main-header/main-header.
       <div class="relative">
         <router-outlet></router-outlet>
       </div>
+
+      <footer class="py-20 text-center w-full text-sm">With 💖 for branjesusdev 🚀</footer>
     </main>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
