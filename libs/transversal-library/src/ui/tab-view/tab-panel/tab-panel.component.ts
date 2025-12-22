98824adcf73bbd1d72fce6f16a3180c0f2a2638c
Brandol Vargas <brandol.fontecha@moviired.co>
2025-12-22 15:41:20 -0500

    Migration angular 20

--- libs/transversal-library/src/ui/tab-view/tab-panel/tab-panel.component.ts
+++ libs/transversal-library/src/ui/tab-view/tab-panel/tab-panel.component.ts
@@ -1,9 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-tab-panel',
-  imports: [CommonModule],
+  imports: [],
   template: `
     @if (active) {
       <div class="animate-fade-left">
